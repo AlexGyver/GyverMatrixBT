36834d4e483ad74dce8b1c39b10ca0900b5ca003
Alex <beragumbo@ya.ru>
2019-01-09 12:54:03 +0300

    add

---
+++ firmware/GyverMatrixOS_v1.11/GyverMatrixOS_v1.11.ino
@@ -0,0 +1,219 @@
+/*
+  Скетч к проекту "Адресная матрица"
+  Гайд по постройке матрицы: https://alexgyver.ru/matrix_guide/
+  Страница проекта (схемы, описания): https://alexgyver.ru/GyverMatrixBT/
+  Подробное описание прошивки: https://alexgyver.ru/gyvermatrixos-guide/
+  Исходники на GitHub: https://github.com/AlexGyver/GyverMatrixBT/
+  Нравится, как написан код? Поддержи автора! https://alexgyver.ru/support_alex/
+  Автор: AlexGyver Technologies, 2018
+  https://AlexGyver.ru/
+*/
+
+// GyverMatrixOS
+// Версия прошивки 1.10, совместима с приложением GyverMatrixBT версии 1.12 и выше
+
+// ************************ МАТРИЦА *************************
+// если прошивка не лезет в Arduino NANO - отключай режимы! Строка 60 и ниже
+
+#define BRIGHTNESS 150        // стандартная маскимальная яркость (0-255)
+#define CURRENT_LIMIT 2000    // лимит по току в миллиамперах, автоматически управляет яркостью (пожалей свой блок питания!) 0 - выключить лимит
+
+#define WIDTH 16              // ширина матрицы
+#define HEIGHT 16             // высота матрицы
+#define SEGMENTS 1            // диодов в одном "пикселе" (для создания матрицы из кусков ленты)
+
+#define COLOR_ORDER GRB       // порядок цветов на ленте. Если цвет отображается некорректно - меняйте. Начать можно с RGB
+
+#define MATRIX_TYPE 0         // тип матрицы: 0 - зигзаг, 1 - параллельная
+#define CONNECTION_ANGLE 0    // угол подключения: 0 - левый нижний, 1 - левый верхний, 2 - правый верхний, 3 - правый нижний
+#define STRIP_DIRECTION 0     // направление ленты из угла: 0 - вправо, 1 - вверх, 2 - влево, 3 - вниз
+// при неправильной настрйоке матрицы вы получите предупреждение "Wrong matrix parameters! Set to default"
+// шпаргалка по настройке матрицы здесь! https://alexgyver.ru/matrix_guide/
+
+#define MCU_TYPE 0            // микроконтроллер: 
+//                            0 - AVR (Arduino NANO/MEGA/UNO)
+//                            1 - ESP8266 (NodeMCU, Wemos D1)
+//                            2 - STM32 (Blue Pill)
+
+// ******************** ЭФФЕКТЫ И РЕЖИМЫ ********************
+#define D_TEXT_SPEED 100      // скорость бегущего текста по умолчанию (мс)
+#define D_EFFECT_SPEED 80     // скорость эффектов по умолчанию (мс)
+#define D_GAME_SPEED 250      // скорость игр по умолчанию (мс)
+#define D_GIF_SPEED 80        // скорость гифок (мс)
+#define DEMO_GAME_SPEED 60    // скорость игр в демо режиме (мс)
+
+boolean AUTOPLAY = 1;         // 0 выкл / 1 вкл автоматическую смену режимов (откл. можно со смартфона)
+int AUTOPLAY_PERIOD = 10;     // время между авто сменой режимов (секунды)
+#define IDLE_TIME 10          // время бездействия кнопок или Bluetooth (в секундах) после которого запускается автосмена режимов и демо в играх
+
+// о поддерживаемых цветах читай тут https://alexgyver.ru/gyvermatrixos-guide/
+#define GLOBAL_COLOR_1 CRGB::Green    // основной цвет №1 для игр
+#define GLOBAL_COLOR_2 CRGB::Orange   // основной цвет №2 для игр
+
+#define SCORE_SIZE 0          // размер символов счёта в игре. 0 - маленький для 8х8 (шрифт 3х5), 1 - большой (шрифт 5х7)
+#define FONT_TYPE 1           // (0 / 1) два вида маленького шрифта в выводе игрового счёта
+
+// ************** ОТКЛЮЧЕНИЕ КОМПОНЕНТОВ СИСТЕМЫ (для экономии памяти) *************
+// внимание! отключение модуля НЕ УБИРАЕТ его эффекты из списка воспроизведения!
+// Это нужно сделать вручную во вкладке custom, удалив ненужные функции
+
+#define USE_BUTTONS 1         // использовать физические кнопки управления играми (0 нет, 1 да)
+#define BT_MODE 1             // использовать блютус (0 нет, 1 да)
+#define USE_NOISE_EFFECTS 1   // крутые полноэкранные эффекты (0 нет, 1 да) СИЛЬНО ЖРУТ ПАМЯТЬ!!!11
+#define USE_FONTS 1           // использовать буквы (бегущая строка) (0 нет, 1 да)
+#define USE_CLOCK 0           // использовать часы (0 нет, 1 да)
+
+// игры
+#define USE_SNAKE 0           // игра змейка (0 нет, 1 да)
+#define USE_TETRIS 0          // игра тетрис (0 нет, 1 да)
+#define USE_MAZE 0            // игра лабиринт (0 нет, 1 да)
+#define USE_RUNNER 0          // игра бегалка-прыгалка (0 нет, 1 да)
+#define USE_FLAPPY 0          // игра flappy bird
+#define USE_ARKAN 0           // игра арканоид
+
+// ****************** ПИНЫ ПОДКЛЮЧЕНИЯ *******************
+// Arduino (Nano, Mega)
+#if (MCU_TYPE == 0)
+#define LED_PIN 6           // пин ленты
+#define BUTT_UP 3           // кнопка вверх
+#define BUTT_DOWN 5         // кнопка вниз
+#define BUTT_LEFT 2         // кнопка влево
+#define BUTT_RIGHT 4        // кнопка вправо
+#define BUTT_SET 7          // кнопка выбор/игра
+
+// пины подписаны согласно pinout платы, а не надписям на пинах!
+// esp8266 - плату выбирал Wemos D1 R1
+#elif (MCU_TYPE == 1)
+#define LED_PIN 2           // пин ленты
+#define BUTT_UP 14          // кнопка вверх
+#define BUTT_DOWN 13        // кнопка вниз
+#define BUTT_LEFT 0         // кнопка влево
+#define BUTT_RIGHT 12       // кнопка вправо
+#define BUTT_SET 15         // кнопка выбор/игра
+
+// STM32 (BluePill) - плату выбирал STM32F103C
+#elif (MCU_TYPE == 2)
+#define LED_PIN PB12         // пин ленты
+#define BUTT_UP PA1          // кнопка вверх
+#define BUTT_DOWN PA3        // кнопка вниз
+#define BUTT_LEFT PA0        // кнопка влево
+#define BUTT_RIGHT PA2       // кнопка вправо
+#define BUTT_SET PA4         // кнопка выбор/игра
+#endif
+
+// ******************************** ДЛЯ РАЗРАБОТЧИКОВ ********************************
+#define DEBUG 0
+#define NUM_LEDS WIDTH * HEIGHT * SEGMENTS
+
+#define RUNNING_STRING 0
+#define CLOCK_MODE 1
+#define GAME_MODE 2
+#define MADNESS_NOISE 3
+#define CLOUD_NOISE 4
+#define LAVA_NOISE 5
+#define PLASMA_NOISE 6
+#define RAINBOW_NOISE 7
+#define RAINBOWSTRIPE_NOISE 8
+#define ZEBRA_NOISE 9
+#define FOREST_NOISE 10
+#define OCEAN_NOISE 11
+#define SNOW_ROUTINE 12
+#define SPARKLES_ROUTINE 13
+#define MATRIX_ROUTINE 14
+#define STARFALL_ROUTINE 15
+#define BALL_ROUTINE 16
+#define BALLS_ROUTINE 17
+#define RAINBOW_ROUTINE 18
+#define RAINBOWDIAGONAL_ROUTINE 19
+#define FIRE_ROUTINE 20
+#define IMAGE_MODE 21
+
+#if (MCU_TYPE == 1)
+#define FASTLED_INTERRUPT_RETRY_COUNT 0
+#define FASTLED_ALLOW_INTERRUPTS 0
+#include <ESP8266WiFi.h>
+#endif
+
+#include "FastLED.h"
+CRGB leds[NUM_LEDS];
+String runningText = "";
+
+static const byte maxDim = max(WIDTH, HEIGHT);
+byte buttons = 4;   // 0 - верх, 1 - право, 2 - низ, 3 - лево, 4 - не нажата
+int globalBrightness = BRIGHTNESS;
+byte globalSpeed = 200;
+uint32_t globalColor = 0x00ff00;   // цвет при запуске зелёный
+byte breathBrightness;
+boolean loadingFlag = true;
+byte frameNum;
+int gameSpeed = DEMO_GAME_SPEED;
+boolean gameDemo = true;
+boolean idleState = true;  // флаг холостого режима работы
+boolean BTcontrol = false;  // флаг контроля с блютус. Если false - управление с кнопок
+int8_t thisMode = 0;
+boolean controlFlag = false;
+boolean gamemodeFlag = false;
+boolean mazeMode = false;
+int effects_speed = D_EFFECT_SPEED;
+int8_t hrs = 10, mins = 25, secs;
+boolean dotFlag;
+byte modeCode;    // 0 бегущая, 1 часы, 2 игры, 3 нойс маднесс и далее, 21 гифка или картинка,
+boolean fullTextFlag = false;
+boolean clockSet = false;
+
+#if (USE_FONTS == 1)
+#include "fonts.h"
+#endif
+
+uint32_t autoplayTime = ((long)AUTOPLAY_PERIOD * 1000);
+uint32_t autoplayTimer;
+
+#include "timerMinim.h"
+timerMinim effectTimer(D_EFFECT_SPEED);
+timerMinim gameTimer(DEMO_GAME_SPEED);
+timerMinim scrollTimer(D_TEXT_SPEED);
+timerMinim idleTimer((long)IDLE_TIME * 1000);
+timerMinim changeTimer(70);
+timerMinim halfsecTimer(500);
+
+#if (USE_CLOCK == 1 && (MCU_TYPE == 0 || MCU_TYPE == 1))
+#include <Wire.h>
+#include "RTClib.h"
+
+RTC_DS3231 rtc;
+// RTC_DS1307 rtc;
+#endif
+
+void setup() {
+#if (BT_MODE == 1)
+  Serial.begin(9600);
+#endif
+
+#if (MCU_TYPE == 1)
+  WiFi.setSleepMode(WIFI_NONE_SLEEP);
+#endif
+
+#if (USE_CLOCK == 1 && (MCU_TYPE == 0 || MCU_TYPE == 1))
+  rtc.begin();
+  if (rtc.lostPower()) {
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  }
+  DateTime now = rtc.now();
+  secs = now.second();
+  mins = now.minute();
+  hrs = now.hour();
+#endif
+
+  // настройки ленты
+  FastLED.addLeds<WS2812, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.setBrightness(BRIGHTNESS);
+  if (CURRENT_LIMIT > 0) FastLED.setMaxPowerInVoltsAndMilliamps(5, CURRENT_LIMIT);
+  FastLED.clear();
+  FastLED.show();
+  randomSeed(analogRead(0) + analogRead(1));    // пинаем генератор случайных чисел
+}
+
+void loop() {
+  customRoutine();
+  bluetoothRoutine();
+}
---
+++ firmware/GyverMatrixOS_v1.11/custom.ino
@@ -0,0 +1,435 @@
+// свой список режимов
+
+// ************************ НАСТРОЙКИ ************************
+#define SMOOTH_CHANGE 1     // плавная смена режимов через чёрный
+#define SHOW_FULL_TEXT 1    // не переключать режим, пока текст не покажется весь
+#define SHOW_TEXT_ONCE 1    // показывать бегущий текст только 1 раз
+
+// подключаем внешние файлы с картинками
+//#include "bitmap2.h"
+
+
+/*
+   Режимы:
+    clockRoutine();       // часы на чёрном фоне
+
+   Эффекты:
+    sparklesRoutine();    // случайные цветные гаснущие вспышки
+    snowRoutine();        // снег
+    matrixRoutine();      // "матрица"
+    starfallRoutine();    // звездопад (кометы)
+    ballRoutine();        // квадратик
+    ballsRoutine();       // шарики
+    rainbowRoutine();     // радуга во всю матрицу горизонтальная
+    rainbowDiagonalRoutine();   // радуга во всю матрицу диагональная
+    fireRoutine();        // огонь
+
+  Крутые эффекты "шума":
+    madnessNoise();       // цветной шум во всю матрицу
+    cloudNoise();         // облака
+    lavaNoise();          // лава
+    plasmaNoise();        // плазма
+    rainbowNoise();       // радужные переливы
+    rainbowStripeNoise(); // полосатые радужные переливы
+    zebraNoise();         // зебра
+    forestNoise();        // шумящий лес
+    oceanNoise();         // морская вода
+
+  Игры:
+    snakeRoutine();     // змейка
+    tetrisRoutine();    // тетрис
+    mazeRoutine();      // лабиринт
+    runnerRoutine();    // бегалка прыгалка
+    flappyRoutine();    // flappy bird
+    arkanoidRoutine();  // арканоид
+
+  Бегущая строка:
+    fillString("Ваш текст", цвет);    // цвет вида 0x00ff25 или CRGB::Red и проч. цвета
+    fillString("Ваш текст", 1);       // радужный перелив текста
+    fillString("Ваш текст", 2);       // каждая буква случайным цветом!
+
+  Рисунки и анимации:
+    loadImage(<название массива>);    // основная функция вывода картинки
+    imageRoutine1();                  // пример использования
+    animation1();                     // пример анимации
+
+*/
+
+// ************************* СВОЙ СПИСОК РЕЖИМОВ ************************
+// список можно менять, соблюдая его структуру. Можно удалять и добавлять эффекты, ставить их в
+// любой последовательности или вообще оставить ОДИН. Удалив остальные case и break. Cтруктура оч простая:
+// case <номер>: <эффект>;
+//  break;
+
+// не забудьте указать количество режимов для корректного переключения с последнего на первый
+#define MODES_AMOUNT 28   // количество кастомных режимов (которые переключаются сами или кнопкой)
+
+void customModes() {
+  switch (thisMode) {
+
+    case 0: fillString("КРАСНЫЙ", CRGB::Red);
+      break;
+    case 1: fillString("РАДУГА", 1);
+      break;
+    case 2: fillString("RGB LED", 2);
+      break;
+    case 3: madnessNoise();
+      break;
+    case 4: cloudNoise();
+      break;
+    case 5: lavaNoise();
+      break;
+    case 6: plasmaNoise();
+      break;
+    case 7: rainbowNoise();
+      break;
+    case 8: rainbowStripeNoise();
+      break;
+    case 9: zebraNoise();
+      break;
+    case 10: forestNoise();
+      break;
+    case 11: oceanNoise();
+      break;
+    case 12: snowRoutine();
+      break;
+    case 13: sparklesRoutine();
+      break;
+    case 14: matrixRoutine();
+      break;
+    case 15: starfallRoutine();
+      break;
+    case 16: ballRoutine();
+      break;
+    case 17: ballsRoutine();
+      break;
+    case 18: rainbowRoutine();
+      break;
+    case 19: rainbowDiagonalRoutine();
+      break;
+    case 20: fireRoutine();
+      break;
+    case 21: snakeRoutine();
+      break;
+    case 22: tetrisRoutine();
+      break;
+    case 23: mazeRoutine();
+      break;
+    case 24: runnerRoutine();
+      break;
+    case 25: flappyRoutine();
+      break;
+    case 26: arkanoidRoutine();
+      break;
+    case 27: clockRoutine();
+      break;
+
+
+  }
+
+}
+
+// функция загрузки картинки в матрицу. должна быть здесь, иначе не работает =)
+void loadImage(uint16_t (*frame)[WIDTH]) {
+  for (byte i = 0; i < WIDTH; i++)
+    for (byte j = 0; j < HEIGHT; j++)
+      drawPixelXY(i, j, gammaCorrection(expandColor((pgm_read_word(&(frame[HEIGHT - j - 1][i]))))));
+  // да, тут происходит лютенький п@здец, а именно:
+  // 1) pgm_read_word - восстанавливаем из PROGMEM (флэш памяти) цвет пикселя в 16 битном формате по его координатам
+  // 2) expandColor - расширяем цвет до 24 бит (спасибо adafruit)
+  // 3) gammaCorrection - проводим коррекцию цвета для более корректного отображения
+}
+timerMinim gifTimer(D_GIF_SPEED);
+
+// ********************** ПРИМЕРЫ ВЫВОДА КАРТИНОК ***********************
+
+// Внимание! Если размер матрицы не совпадает с исходным размером матрицы в скетче
+// (если вы только что  его скачали), то нужно удалить/закомментировать данные функции!
+//
+/*
+  // показать картинку
+  void imageRoutine1() {
+  if (loadingFlag) {
+    loadingFlag = false;
+    loadImage(frame00);
+  }
+  }
+
+  void animation1() {
+  if (gifTimer.isReady()) {
+    frameNum++;
+    if (frameNum >= sizeof(framesArray)) frameNum = 0;
+    loadImage(framesArray[frameNum]);
+  }
+  }
+*/
+
+// ********************* ОСНОВНОЙ ЦИКЛ РЕЖИМОВ *******************
+#if (SMOOTH_CHANGE == 1)
+byte fadeMode = 4;
+boolean modeDir;
+#endif
+
+static void nextMode() {
+#if (SMOOTH_CHANGE == 1)
+  fadeMode = 0;
+  modeDir = true;
+#else
+  nextModeHandler();
+#endif
+}
+static void prevMode() {
+#if (SMOOTH_CHANGE == 1)
+  fadeMode = 0;
+  modeDir = false;
+#else
+  prevModeHandler();
+#endif
+}
+void nextModeHandler() {
+  thisMode++;
+  if (thisMode >= MODES_AMOUNT) thisMode = 0;
+  loadingFlag = true;
+  gamemodeFlag = false;
+  FastLED.clear();
+  FastLED.show();
+}
+void prevModeHandler() {
+  thisMode--;
+  if (thisMode < 0) thisMode = MODES_AMOUNT - 1;
+  loadingFlag = true;
+  gamemodeFlag = false;
+  FastLED.clear();
+  FastLED.show();
+}
+
+int fadeBrightness;
+#if (SMOOTH_CHANGE == 1)
+void modeFader() {
+  if (fadeMode == 0) {
+    fadeMode = 1;
+  } else if (fadeMode == 1) {
+    if (changeTimer.isReady()) {
+      fadeBrightness -= 40;
+      if (fadeBrightness < 0) {
+        fadeBrightness = 0;
+        fadeMode = 2;
+      }
+      FastLED.setBrightness(fadeBrightness);
+    }
+  } else if (fadeMode == 2) {
+    fadeMode = 3;
+    if (modeDir) nextModeHandler();
+    else prevModeHandler();
+  } else if (fadeMode == 3) {
+    if (changeTimer.isReady()) {
+      fadeBrightness += 40;
+      if (fadeBrightness > globalBrightness) {
+        fadeBrightness = globalBrightness;
+        fadeMode = 4;
+      }
+      FastLED.setBrightness(fadeBrightness);
+    }
+  }
+}
+#endif
+
+boolean loadFlag2;
+void customRoutine() {
+  if (!BTcontrol) {
+    if (!gamemodeFlag) {
+      if (effectTimer.isReady()) {
+#if (OVERLAY_CLOCK == 1 && USE_CLOCK == 1)
+        if (overlayAllowed()) {
+          if (!loadingFlag && !gamemodeFlag && needUnwrap() && modeCode != 0) clockOverlayUnwrap(CLOCK_X, CLOCK_Y);
+          if (loadingFlag) loadFlag2 = true;
+        }
+#endif
+
+        customModes();                // режимы крутятся, пиксели мутятся
+
+#if (OVERLAY_CLOCK == 1 && USE_CLOCK == 1)
+        if (overlayAllowed()) {
+          if (!gamemodeFlag && modeCode != 0) clockOverlayWrap(CLOCK_X, CLOCK_Y);
+          if (loadFlag2) {
+            setOverlayColors();
+            loadFlag2 = false;
+          }
+        }
+#endif
+        loadingFlag = false;
+        FastLED.show();
+      }
+    } else {
+      customModes();
+    }
+    btnsModeChange();
+#if (SMOOTH_CHANGE == 1)
+    modeFader();
+#endif
+  }
+
+  if (idleState) {
+    if (fullTextFlag && SHOW_TEXT_ONCE) {
+      fullTextFlag = false;
+      autoplayTimer = millis();
+      nextMode();
+    }
+    if (millis() - autoplayTimer > autoplayTime && AUTOPLAY) {    // таймер смены режима
+      if (modeCode == 0 && SHOW_FULL_TEXT) {    // режим текста
+        if (fullTextFlag) {
+          fullTextFlag = false;
+          autoplayTimer = millis();
+          nextMode();
+        }
+      } else {
+        autoplayTimer = millis();
+        nextMode();
+      }
+    }
+  } else {
+    if (idleTimer.isReady()) {      // таймер холостого режима
+      idleState = true;
+      autoplayTimer = millis();
+      gameDemo = true;
+
+      gameSpeed = DEMO_GAME_SPEED;
+      gameTimer.setInterval(gameSpeed);
+
+      loadingFlag = true;
+      BTcontrol = false;
+      FastLED.clear();
+      FastLED.show();
+    }
+  }
+}
+
+void timeSet(boolean type, boolean dir) {    // type: 0-часы, 1-минуты, dir: 0-уменьшить, 1-увеличить
+  if (type) {
+    if (dir) hrs++;
+    else hrs--;
+  } else {
+    if (dir) mins++;
+    else mins--;
+    if (mins > 59) {
+      mins = 0;
+      hrs++;
+    }
+    if (mins < 0) {
+      mins = 59;
+      hrs--;
+    }
+  }
+  if (hrs > 23) hrs = 0;
+  if (hrs < 0) hrs = 23;
+}
+
+void btnsModeChange() {
+#if (USE_BUTTONS == 1)
+  if (bt_set.clicked()) {
+    if (gamemodeFlag) gameDemo = !gameDemo;
+    if (gameDemo) {
+      gameSpeed = DEMO_GAME_SPEED;
+      gameTimer.setInterval(gameSpeed);
+      AUTOPLAY = true;
+    } else {
+      gameSpeed = D_GAME_SPEED;
+      gameTimer.setInterval(gameSpeed);
+      AUTOPLAY = false;
+    }
+  }
+  if (bt_set.holded()) {
+    if (modeCode == 2)
+      mazeMode = !mazeMode;
+    if (modeCode == 1) {    // вход в настройку часов
+      clockSet = !clockSet;
+      AUTOPLAY = false;
+      secs = 0;
+#if (USE_CLOCK == 1)
+      if (!clockSet) rtc.adjust(DateTime(2014, 1, 21, hrs, mins, 0)); // установка нового времени в RTC
+#endif
+    }
+  }
+
+  // timeSet type: 0-часы, 1-минуты, dir: 0-уменьшить, 1-увеличить
+
+  if (gameDemo) {
+    if (bt_right.clicked()) {
+      if (!clockSet) {
+        autoplayTimer = millis();
+        nextMode();
+      } else {
+        timeSet(1, 1);
+      }
+    }
+
+    if (bt_left.clicked()) {
+      if (!clockSet) {
+        autoplayTimer = millis();
+        prevMode();
+      } else {
+        timeSet(1, 0);
+      }
+    }
+
+    if (bt_up.clicked()) {
+      if (!clockSet) {
+        AUTOPLAY = true;
+        autoplayTimer = millis();
+      } else {
+        timeSet(0, 1);
+      }
+    }
+    if (bt_down.clicked()) {
+      if (!clockSet) {
+        AUTOPLAY = false;
+      } else {
+        timeSet(0, 0);
+      }
+    }
+
+    if (bt_right.holding())
+      if (changeTimer.isReady()) {
+        if (!clockSet) {
+          effects_speed -= 2;
+          if (effects_speed < 30) effects_speed = 30;
+          effectTimer.setInterval(effects_speed);
+        } else {
+          timeSet(1, 1);
+        }
+      }
+    if (bt_left.holding())
+      if (changeTimer.isReady()) {
+        if (!clockSet) {
+          effects_speed += 2;
+          if (effects_speed > 300) effects_speed = 300;
+          effectTimer.setInterval(effects_speed);
+        } else {
+          timeSet(1, 0);
+        }
+      }
+    if (bt_up.holding())
+      if (changeTimer.isReady()) {
+        if (!clockSet) {
+          globalBrightness += 2;
+          if (globalBrightness > 255) globalBrightness = 255;
+          fadeBrightness = globalBrightness;
+          FastLED.setBrightness(globalBrightness);
+        } else {
+          timeSet(0, 1);
+        }
+      }
+    if (bt_down.holding())
+      if (changeTimer.isReady()) {
+        if (!clockSet) {
+          globalBrightness -= 2;
+          if (globalBrightness < 0) globalBrightness = 0;
+          fadeBrightness = globalBrightness;
+          FastLED.setBrightness(globalBrightness);
+        } else {
+          timeSet(0, 0);
+        }
+      }
+  }
+#endif
+}

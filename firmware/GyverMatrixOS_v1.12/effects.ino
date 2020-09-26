da870937c2d7fa442600be7f405b7a1740a949c0
Stepko/ <sguralmen@gmail.com>
2020-09-26 21:27:13 +0300

    + Новый огонь - 7% потребления оперативки

--- firmware/GyverMatrixOS_v1.12/effects.ino
+++ firmware/GyverMatrixOS_v1.12/effects.ino
@@ -10,9 +10,7 @@
 #define BALL_SIZE 3       // размер шара
 
 // эффект "огонь"
-#define SPARKLES 1        // вылетающие угольки вкл выкл
-#define HUE_ADD 0         // добавка цвета в огонь (от 0 до 230) - меняет весь цвет пламени
-
+#define FSCALE 1+30       //масштаб
 // эффект "кометы"
 #define TAIL_STEP 100     // длина хвоста кометы
 #define SATURATION 150    // насыщенность кометы (от 0 до 255) 0=Метель
@@ -151,122 +149,18 @@ void rainbowColorsRoutine() {
 
 
 // ********************** огонь **********************
-uint8_t pcnt;
-uint8_t line[WIDTH];
-unsigned char matrixValue[8][16];
-//these values are substracetd from the generated values to give a shape to the animation
-const unsigned char valueMask[8][16] PROGMEM = {
-  {0  , 0  , 0  , 32 , 32 , 0  , 0  , 0  , 0  , 0  , 0  , 32 , 32 , 0  , 0  , 0  },
-  {0  , 0  , 0  , 64 , 64 , 0  , 0  , 0  , 0  , 0  , 0  , 64 , 64 , 0  , 0  , 0  },
-  {0  , 0  , 32 , 96 , 96 , 32 , 0  , 0  , 0  , 0  , 32 , 96 , 96 , 32 , 0  , 0  },
-  {0  , 32 , 64 , 128, 128, 64 , 32 , 0  , 0  , 32 , 64 , 128, 128, 64 , 32 , 0  },
-  {32 , 64 , 96 , 160, 160, 96 , 64 , 32 , 32 , 64 , 96 , 160, 160, 96 , 64 , 32 },
-  {64 , 96 , 128, 192, 192, 128, 96 , 64 , 64 , 96 , 128, 192, 192, 128, 96 , 64 },
-  {96 , 128, 160, 255, 255, 160, 128, 96 , 96 , 128, 160, 255, 255, 160, 128, 96 },
-  {128, 160, 192, 255, 255, 192, 160, 128, 128, 160, 192, 255, 255, 192, 160, 128}
-};
-
-//these are the hues for the fire,
-//should be between 0 (red) to about 25 (yellow)
-const unsigned char hueMask[8][16] PROGMEM = {
-  {25, 22, 11, 1 , 1 , 11, 19, 25, 25, 22, 11, 1 , 1 , 11, 19, 25 },
-  {25, 19, 8 , 1 , 1 , 8 , 13, 19, 25, 19, 8 , 1 , 1 , 8 , 13, 19 },
-  {19, 16, 8 , 1 , 1 , 8 , 13, 16, 19, 16, 8 , 1 , 1 , 8 , 13, 16 },
-  {13, 13, 5 , 1 , 1 , 5 , 11, 13, 13, 13, 5 , 1 , 1 , 5 , 11, 13 },
-  {11, 11, 5 , 1 , 1 , 5 , 11, 11, 11, 11, 5 , 1 , 1 , 5 , 11, 11 },
-  {8 , 5 , 1 , 0 , 0 , 1 , 5 , 8 , 8 , 5 , 1 , 0 , 0 , 1 , 5 , 8  },
-  {5 , 1 , 0 , 0 , 0 , 0 , 1 , 5 , 5 , 1 , 0 , 0 , 0 , 0 , 1 , 5  },
-  {1 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 1  }
-};
 void fireRoutine() {
-  if (loadingFlag) {
-    loadingFlag = false;
-    modeCode=20;
-    // memset8( leds, 0, NUM_LEDS * 3) ;
-    generateLine();
-  }
-  if (pcnt >= 100) {
-    shiftup();
-    generateLine();
-    pcnt = 0;
-  }
-  drawFrame(pcnt);
-  pcnt += 25;
-}
+  modeCode=20;
+  //CRGBPalette16 myPal = firepal;
 
-// Случайным образом генерирует следующую линию (matrix row)
+  
 
-void generateLine() {
-  for (uint8_t x = 0; x < WIDTH; x++) {
-    line[x] = random(127, 255);
-  }
-}
-
-void shiftup() {
-  for (uint8_t y = HEIGHT - 1; y > 0; y--) {
-    for (uint8_t x = 0; x < WIDTH; x++) {
-      uint8_t newX = x;
-      if (x > 15) newX = x - 15;
-      if (y > 7) continue;
-      matrixValue[y][newX] = matrixValue[y - 1][newX];
+  uint32_t a = millis();
+  for (byte i = 0U; i < WIDTH; i++) {
+    for (float j = 0.; j < HEIGHT; j++) {
+//      if(curPalette!=palettes.at(10))
+        drawPixelXY((WIDTH - 1) - i, (HEIGHT - 1) - j, ColorFromPalette(HeatColors_p, qsub8(inoise8(i * FSCALE, j * FSCALE + a, a / 255), abs8(j - (HEIGHT - 1)) * 255 / (HEIGHT - 1)), 255));
     }
-  }
-
-  for (uint8_t x = 0; x < WIDTH; x++) {
-    uint8_t newX = x;
-    if (x > 15) newX = x - 15;
-    matrixValue[0][newX] = line[newX];
-  }
-}
-
-// рисует кадр, интерполируя между 2 "ключевых кадров"
-// параметр pcnt - процент интерполяции
-
-void drawFrame(int pcnt) {
-  int nextv;
-
-  //each row interpolates with the one before it
-  for (unsigned char y = HEIGHT - 1; y > 0; y--) {
-    for (unsigned char x = 0; x < WIDTH; x++) {
-      uint8_t newX = x;
-      if (x > 15) newX = x - 15;
-      if (y < 8) {
-        nextv =
-          (((100.0 - pcnt) * matrixValue[y][newX]
-            + pcnt * matrixValue[y - 1][newX]) / 100.0)
-          - pgm_read_byte(&(valueMask[y][newX]));
-
-        CRGB color = CHSV(
-                       HUE_ADD + pgm_read_byte(&(hueMask[y][newX])), // H
-                       255, // S
-                       (uint8_t)max(0, nextv) // V
-                     );
-
-        leds[getPixelNumber(x, y)] = color;
-      } else if (y == 8 && SPARKLES) {
-        if (random(0, 20) == 0 && getPixColorXY(x, y - 1) != 0) drawPixelXY(x, y, getPixColorXY(x, y - 1));
-        else drawPixelXY(x, y, 0);
-      } else if (SPARKLES) {
-
-        // старая версия для яркости
-        if (getPixColorXY(x, y - 1) > 0)
-          drawPixelXY(x, y, getPixColorXY(x, y - 1));
-        else drawPixelXY(x, y, 0);
-
-      }
-    }
-  }
-
-  //Перавя стрка интерполируется со следующей "next" линией
-  for (unsigned char x = 0; x < WIDTH; x++) {
-    uint8_t newX = x;
-    if (x > 15) newX = x - 15;
-    CRGB color = CHSV(
-                   HUE_ADD + pgm_read_byte(&(hueMask[0][newX])), // H
-                   255,           // S
-                   (uint8_t)(((100.0 - pcnt) * matrixValue[0][newX] + pcnt * line[newX]) / 100.0) // V
-                 );
-    leds[getPixelNumber(newX, 0)] = color;
   }
 }
 // **************** МАТРИЦА *****************

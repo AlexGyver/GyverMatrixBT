437eda7294b43500f3543475151ee79639710621
Stepko/ <sguralmen@gmail.com>
2021-06-29 22:11:32 +0300

    Deleting OLD VERSIONS

--- firmware/GyverMatrixOS_v1.13/noise_effects.ino
+++ firmware/GyverMatrixOS_v1.13/noise_effects.ino
@@ -137,68 +137,16 @@ void lavaNoise() {
   fillNoiseLED();
 }
 
-// ******************* СЛУЖЕБНЫЕ *******************
-void fillNoiseLED() {
-  uint8_t dataSmoothing = 0;
-  if ( speed < 50) {
-    dataSmoothing = 200 - (speed * 4);
-  }
-  for (int i = 0; i < MAX_DIMENSION; i++) {
-    int ioffset = scale * i;
-    for (int j = 0; j < MAX_DIMENSION; j++) {
-      int joffset = scale * j;
-
-      uint8_t data = inoise8(x + ioffset, y + joffset, z);
-
-      data = qsub8(data, 16);
-      data = qadd8(data, scale8(data, 39));
-
-      if ( dataSmoothing ) {
-        uint8_t olddata = noise[i][j];
-        uint8_t newdata = scale8( olddata, dataSmoothing) + scale8( data, 256 - dataSmoothing);
-        data = newdata;
-      }
-
-      noise[i][j] = data;
+void FillNoise(byte Speed, byte Scale, bool ShiftX, bool ShiftY, bool ShiftZ, CRGBPalette16 palette, bool ShiftHue, bool BriNoise) {
+ double t = (millis() / (map(Speed, 1, 255, 60, 0)));
+  for (byte x = 0; x < WIDTH; x++) {
+    for (byte y = 0; y < HEIGHT; y++) {
+      uint8_t noise = inoise8((x * Scale) + ((ShiftX) ? t : t / 8), (y * Scale) + ((ShiftY) ? t : t / 16), ((ShiftZ) ? t : 0));
+      uint8_t Inoise = inoise8((y * Scale) + ((ShiftY) ? t : t / 16), (x * Scale) + ((ShiftX) ? t : t / 8), ((ShiftZ) ? t : 0));
+      leds[XY(x, y)] = ColorFromPalette(palette, noise + hue,(!BriNoise || Inoise > 127)? 255 : dim8_raw(Inoise * 2));
     }
   }
-  z += speed;
-
-  // apply slow drift to X and Y, just for visual variation.
-  x += speed / 8;
-  y -= speed / 16;
-
-  for (int i = 0; i < WIDTH; i++) {
-    for (int j = 0; j < HEIGHT; j++) {
-      uint8_t index = noise[j][i];
-      uint8_t bri =   noise[i][j];
-      // if this palette is a 'loop', add a slowly-changing base value
-      if ( colorLoop) {
-        index += ihue;
-      }
-      // brighten up, as the color palette itself often contains the
-      // light/dark dynamic range desired
-      if ( bri > 127 ) {
-        bri = 255;
-      } else {
-        bri = dim8_raw( bri * 2);
-      }
-      CRGB color = ColorFromPalette( currentPalette, index, bri);
-      drawPixelXY(i, j, color);   //leds[getPixelNumber(i, j)] = color;
-    }
-  }
-  ihue += 1;
-}
-
-void fillnoise8() {
-  for (int i = 0; i < MAX_DIMENSION; i++) {
-    int ioffset = scale * i;
-    for (int j = 0; j < MAX_DIMENSION; j++) {
-      int joffset = scale * j;
-      noise[i][j] = inoise8(x + ioffset, y + joffset, z);
-    }
-  }
-  z += speed;
+  if (ShiftHue) { hue++; } else { hue = 0; }
 }
 
 #else
